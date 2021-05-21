d6a1211337e637f709bbae59981994092122237b
Griko Nibras <git@griko.id>
2021-05-21 17:04:30 +0700

    feat(ga): implement gen indiv and population static method

--- c2ga/genetic_algorithm.py
+++ c2ga/genetic_algorithm.py
@@ -1,5 +1,7 @@
 from typing import Callable
 
+import numpy as np
+
 
 class GeneticAlgorithm:
     crossover_rate: float
@@ -15,3 +17,18 @@ class GeneticAlgorithm:
         self.gene_count = count
         self.generator = generator
         self.objective_fn = fn
+
+    @staticmethod
+    def fitness():
+        # TODO
+        return
+
+    @staticmethod
+    def generate_individual(size: int, low: float = 0.0, high: float = 1.0):
+        return np.random.uniform(low, high, size)
+
+    @staticmethod
+    def generate_population(count: int, size: int, low: float = 0.0, high: float = 1.0):
+        return np.array([
+            GeneticAlgorithm.generate_individual(size, low, high) for _ in range(count)
+        ])
