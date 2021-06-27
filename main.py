8fcc94c63354a22a4500ebbbf187d6e0c9e7b47e
Griko Nibras <git@griko.id>
2021-06-27 15:39:26 +0700

    chore(main): reorganize layout

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ st.set_page_config(
 
 # -----------------------------------------------------------------------------
 
-st.sidebar.header('Parameters')
+st.sidebar.write('''
+## Parameters
+''')
 
 dataset_select = st.sidebar.selectbox(
     'Select dataset',
@@ -25,70 +27,85 @@ dataset_select = st.sidebar.selectbox(
 a = st.sidebar.number_input('A', value=2.94)
 b = st.sidebar.number_input('B', value=0.91)
 
-# -----------------------------------------------------------------------------
-
 source = pd.read_csv(dataset_select, sep=';')
-c = Cocomo2(source, a=a, b=b)
+c = Cocomo2(source, a, b)
 
 # -----------------------------------------------------------------------------
 
-'''
-# main.py
+with st.beta_container():
+    st.write(f'''
+    # Optimasi Metode COCOMO II Menggunakan Algoritma Genetika
+    
+    <https://griko.dev/undergrad-py> · <https://github.com/grikomsn/undergrad-thesis>
+    
+    ## Load dataset
+    
+    Using `{dataset_select}` dataset (change on sidebar)
+    ''')
 
-Optimasi Metode COCOMO II Menggunakan Algoritma Genetika
+    st.write(pd.DataFrame(c.data))
 
-<https://griko.dev/undergrad-py> · <https://github.com/grikomsn/undergrad-thesis>
-'''
+# -----------------------------------------------------------------------------
 
-'---'
+with st.beta_container():
+    st.write('''
+    ## Compute EM, EE, MRE, MMRE
+    
+    - estimated effort = objective function -> F(A,B)
+    - SF = scale factor
+    ''')
 
-st.header('Load dataset')
-st.write(f'Using `{dataset_select}` dataset (change on sidebar)')
-st.write(c.data)
+    left, right = st.beta_columns(2)
 
-'---'
+    left.write(r'''
+    ### Effort multiplier (EM)
+    
+    $$
+    EM = \prod_{i=1}^{17} SF_i
+    $$
+    ''')
 
-'''
-## Compute EM, EE, MRE, MMRE
+    left.write(
+        pd.DataFrame(c.effort_multipliers, columns=['EM'])
+    )
 
-- estimated effort = objective function -> F(A,B)
-- SF = scale factor
-'''
+    right.write(r'''
+    ### Estimated effort (EE)
+    
+    $$
+    F(A,B) = A \times Size^B \times EM
+    $$
+    ''')
+
+    right.write(
+        pd.DataFrame(c.estimated_efforts, columns=['F(A,B)'])
+    )
+
+# -----------------------------------------------------------------------------
 
 with st.beta_container():
     left, right = st.beta_columns(2)
 
-    left.subheader('Effort multiplier (EM)')
-    left.latex(r'''
-        EM = \prod_{i=1}^{17} SF_i
+    left.write(r'''
+    ### Magnitude relative error (MRE)
+    
+    $$
+    MRE = \frac{|AE-EE|}{EE} \times 100\%
+    $$
     ''')
+
     left.write(
-        pd.DataFrame(c.effort_multipliers, columns=["EM"])
+        pd.DataFrame(c.mre * 100, columns=['MRE'])
     )
 
-    right.subheader('Estimated effort (EE)')
-    right.latex(r'''
-        F(A,B) = A \times Size^B \times EM
+    right.write(r'''
+    ### Mean magnitude relative error (MMRE)
+    
+    $$
+    MMRE = \frac{1}{N} \sum_{x=1}^{N} MRE
+    $$
     ''')
+
     right.write(
-        pd.DataFrame(c.estimated_efforts, columns=["F(A,B)"])
-    )
-
-with st.beta_container():
-    left, right = st.beta_columns(2)
-
-    left.subheader('Magnitude relative error (MRE)')
-    left.latex(r'''
-        MRE = \frac{|AE-EE|}{EE} \times 100\%
-    ''')
-    left.write(
-        pd.DataFrame(c.magnitude_relative_error * 100, columns=["MRE"])
-    )
-
-    right.subheader('Mean magnitude relative error (MMRE)')
-    right.latex(r'''
-        MMRE = \frac{1}{N} \sum_{x=1}^{N} MRE
-    ''')
-    right.write(
-        pd.DataFrame([c.mean_magnitude_relative_error * 100], columns=["MMRE"])
+        pd.DataFrame([c.mmre * 100], columns=['MMRE'])
     )
