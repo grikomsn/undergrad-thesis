8153707347ae770d606c4c6240d909402f01e307
Griko Nibras <git@griko.id>
2021-06-27 20:47:01 +0700

    refactor(main): reorganize structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,109 +3,80 @@ import streamlit as st
 
 from c2ga import Cocomo2
 
-# -----------------------------------------------------------------------------
+TITLE = 'Optimasi Metode COCOMO II Menggunakan Algoritma Genetika'
 
-st.set_page_config(
-    page_title='Optimasi Metode COCOMO II Menggunakan Algoritma Genetika',
-    page_icon='👨‍💻',
-)
 
-# -----------------------------------------------------------------------------
+def main():
+    st.set_page_config(page_title=TITLE, page_icon='👨‍💻')
+    st.title(TITLE)
+    st.write('<https://griko.dev/undergrad-py> · <https://github.com/grikomsn/undergrad-thesis> \n ---')
+    run_app()
 
-st.sidebar.write('''
-## Parameters
-''')
 
-dataset_select = st.sidebar.selectbox(
-    'Select dataset',
-    [
-        'turkish.csv'
-        # 'nasa93.csv',
-    ]
-)
+@st.cache()
+def create_cocomo(source: str, a: float, b: float):
+    data = pd.read_csv(source, sep=';')
+    return Cocomo2(data, a, b)
 
-a = st.sidebar.number_input('A', value=2.94)
-b = st.sidebar.number_input('B', value=0.91)
 
-source = pd.read_csv(dataset_select, sep=';')
-c = Cocomo2(source, a, b)
+def run_app():
+    a: float
+    b: float
+    dataset_select: str
 
-# -----------------------------------------------------------------------------
+    with st.sidebar.beta_container():
+        st.write('## Parameters')
+        dataset_select = st.selectbox(
+            'Select dataset',
+            [
+                'turkish.csv'
+                # 'nasa93.csv',
+            ]
+        )
+        a = st.number_input('A', value=2.94)
+        b = st.number_input('B', value=0.91)
 
-with st.beta_container():
-    st.write(f'''
-    # Optimasi Metode COCOMO II Menggunakan Algoritma Genetika
-    
-    <https://griko.dev/undergrad-py> · <https://github.com/grikomsn/undergrad-thesis>
-    
-    ## Load dataset
-    
-    Using `{dataset_select}` dataset (change on sidebar)
-    ''')
+    c = create_cocomo(dataset_select, a, b)
 
-    st.write(pd.DataFrame(c.data))
+    st.write(f'## Load dataset \n Currently using `{dataset_select}` dataset (change on sidebar)')
+    st.dataframe(c.data)
 
-# -----------------------------------------------------------------------------
+    '---'
 
-with st.beta_container():
-    st.write('''
-    ## Compute EM, EE, MRE, MMRE
-    
-    - estimated effort = objective function -> F(A,B)
-    - SF = scale factor
-    ''')
+    st.write('## Compute EM, EE, MRE')
+    with st.beta_container():
+        left, right = st.beta_columns(2)
 
-    left, right = st.beta_columns(2)
+        left.write('### Effort multiplier (EM)')
+        left.latex(r'EM = \prod_{i=1}^{17} SF_i')
+        left.write('> SF = scale factor')
 
-    left.write(r'''
-    ### Effort multiplier (EM)
-    
-    $$
-    EM = \prod_{i=1}^{17} SF_i
-    $$
-    ''')
+        left.write('### Estimated effort (EE)')
+        left.latex(r'F(A,B) = A \times Size^B \times EM')
+        left.write('> EE = objective function -> F(A,B)')
 
-    left.write(
-        pd.DataFrame(c.effort_multipliers, columns=['EM'])
-    )
+        left.write('### Magnitude relative error (MRE)')
+        left.latex(r'MRE = \frac{|AE-EE|}{EE} \times 100\%')
 
-    right.write(r'''
-    ### Estimated effort (EE)
-    
-    $$
-    F(A,B) = A \times Size^B \times EM
-    $$
-    ''')
+        right.write('### Results')
+        right.dataframe({
+            'EM': c.effort_multipliers,
+            'F(A,B)': c.estimated_efforts,
+            'MRE': c.mre * 100,
+        })
 
-    right.write(
-        pd.DataFrame(c.estimated_efforts, columns=['F(A,B)'])
-    )
+        st.write('---')
 
-# -----------------------------------------------------------------------------
+    st.write('## Compute MMRE')
+    with st.beta_container():
+        left, right = st.beta_columns([2, 1])
 
-with st.beta_container():
-    left, right = st.beta_columns(2)
+        left.write('### Mean magnitude relative error (MMRE)')
+        left.latex(r'MMRE = \frac{1}{N} \sum_{x=1}^{N} MRE')
 
-    left.write(r'''
-    ### Magnitude relative error (MRE)
-    
-    $$
-    MRE = \frac{|AE-EE|}{EE} \times 100\%
-    $$
-    ''')
+        right.write('### Results')
+        right.write(pd.DataFrame({'MMRE': [c.mmre * 100]}))
 
-    left.write(
-        pd.DataFrame(c.mre * 100, columns=['MRE'])
-    )
 
-    right.write(r'''
-    ### Mean magnitude relative error (MMRE)
-    
-    $$
-    MMRE = \frac{1}{N} \sum_{x=1}^{N} MRE
-    $$
-    ''')
-
-    right.write(
-        pd.DataFrame([c.mmre * 100], columns=['MMRE'])
-    )
+if __name__ == '__main__':
+    main()
